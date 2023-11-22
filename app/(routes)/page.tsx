854f10e7445ef17eab4e2053ec3d4ba3460abd90
Dorota Leniec <dorota.leniec.dev@gmail.com>
2023-11-22 17:11:22 +0100

    Add Billboard page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return ( <div>Hello Store</div> );
-}
- 
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("cade0acb-5c4c-4484-90ce-1987551ec7fb");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
